ed7b91fe77cd652f55973ffc9d537e279b0b6497
andrevnl <andrevnl@hotmail.com>
2022-03-19 18:45:09 -0300

    Parte 3 - Curso Alura

--- src/main/java/br/com/alura/spring/data/SpringDataApplication.java
+++ src/main/java/br/com/alura/spring/data/SpringDataApplication.java
@@ -2,7 +2,10 @@ package br.com.alura.spring.data;
 
 import br.com.alura.spring.data.orm.Cargo;
 import br.com.alura.spring.data.repository.CargoRepository;
+import br.com.alura.spring.data.repository.FuncionarioRepository;
 import br.com.alura.spring.data.service.CrudCargoService;
+import br.com.alura.spring.data.service.CrudFuncionarioService;
+import br.com.alura.spring.data.service.RelatoriosService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,11 +16,15 @@ import java.util.Scanner;
 public class SpringDataApplication implements CommandLineRunner {
 
 	private final CrudCargoService cargoService;
+	private final RelatoriosService relatoriosService;
+	private final CrudFuncionarioService funcionarioService;
 
-	private Boolean system = true;
+	private final Boolean system = true;
 
-	public SpringDataApplication(CrudCargoService crudCargoService) {
+	public SpringDataApplication(CrudCargoService crudCargoService, RelatoriosService relatoriosService, CrudFuncionarioService funcionarioService) {
 		this.cargoService = crudCargoService;
+		this.relatoriosService = relatoriosService;
+		this.funcionarioService = funcionarioService;
 	}
 
 	public static void main(String[] args) {
@@ -32,14 +39,25 @@ public class SpringDataApplication implements CommandLineRunner {
 			System.out.println("Qual ação vc quer executar");
 			System.out.println("0 - Sair");
 			System.out.println("1 - Cargo");
+			System.out.println("2 - Relatorios");
+			System.out.println("3 - Funcionarios");
 
 			int action = scanner.nextInt();
-			if (action == 1) {
-				cargoService.inicial(scanner);
-			} else if(action == 0) {
-				system = false;
-				System.exit(0);
+
+			switch (action) {
+				case 1:
+					cargoService.inicial(scanner);
+					break;
+				case 2:
+					relatoriosService.inicial(scanner);
+					break;
+				case 3:
+					funcionarioService.inicial(scanner);
+					break;
+				default:
+					break;
 			}
+
 		}
 
 	}
